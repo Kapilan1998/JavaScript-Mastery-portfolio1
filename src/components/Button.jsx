8b4b5f94ae17cedb4e1e3db8c7fe6bec4e93792c
Kapilan <sriranjankapilan@gmail.com>
2025-10-25 13:55:10 +0530

    onclick event added for button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
-const Button = ({text,className,id}) => {
+const Button = ({ text, className, id }) => {
     return (
-        <a className={`${className ?? ''} cta-wrapper`}>
+        <a
+            onClick={(e) => {
+                e.preventDefault();
+                const target = document.getElementById('counter');
+                if (target && id) {
+                    const offset = window.innerWidth * 0.15;
+                    const top = target.getBoundingClientRect().top + window.screenY - offset;
+                    window.scrollTo({
+                        top: top,
+                        behavior: 'smooth'
+                    });
+                }
+            }}
+            className={`${className ?? ''} cta-wrapper`}>
             <div className='cta-button group'>
                 <div className='bg-circle' />
                 <p className='text'>{text}</p>
