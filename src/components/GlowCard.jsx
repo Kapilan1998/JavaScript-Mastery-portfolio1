39ccb4bc47af141dece577feac26b4b59be2bfdb
Kapilan <sriranjankapilan@gmail.com>
2025-11-04 08:29:25 +0530

    children typo error correctd as image log was not displayed

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const GlowCard = ({ card, childern }) => {
+const GlowCard = ({ card, children }) => {
+    
     return (
         <div className='card card-border timeline-card rounded-xl p-10'>
             <div className='glow' />
@@ -12,8 +13,8 @@ const GlowCard = ({ card, childern }) => {
 
             <div className='mb-5'>
                 <p className='text-white-50 text-lg'>{card.review}</p>
-            </div>
-            {childern}
+            </div>            
+            {children}
         </div>
     )
 }
