fcacc3c687d57029168388f84429ccc41ae8253c
Kapilan <sriranjankapilan@gmail.com>
2025-11-07 07:32:05 +0530

    experience section annimated with gsap effect

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -2,8 +2,56 @@ import React from 'react'
 import TitleHeader from '../components/TitleHeader'
 import { expCards } from '../constants'
 import GlowCard from '../components/GlowCard'
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const ExperienceSection = () => {
+    useGSAP(() => {
+        gsap.utils.toArray('.timeline-card').forEach((card) => {
+            gsap.from(card, {
+                xPercent: -100,
+                opacity: 0,
+                duration: 2,
+                transformOrigin: 'left left',
+                ease: 'power2.inOut',
+                scrollTrigger: {
+                    trigger: card,
+                    start: 'top 80%'
+                }
+            })
+        });
+
+        gsap.to('.timeline', {
+            transformOrigin: 'bottom bottom',
+            ease: 'power1.inOut',
+            scrollTrigger: {
+                trigger: '.timeline',
+                start: 'top center',
+                end: '70% bottom',
+                onUpdate: (self) => {
+                    gsap.to('.timeline', {
+                        scaleY: 1 - self.progress
+                    })
+                }
+            },
+        })
+
+        gsap.utils.toArray('.expText').forEach((text) => {
+            gsap.from(text, {
+                xPercent: 0,
+                opacity: 0,
+                duration: 1,
+                ease: 'power2.inOut',
+                scrollTrigger: {
+                    trigger: text,
+                    start: 'top 60%'
+                }
+            })
+        });
+    }, []);
     return (
         <section id="experience" className='w-full md:mt-40 mt-20 section-padding xl:px-0'>
             <div className='w-full h-full md:px-20 px-5'>
@@ -40,7 +88,7 @@ const ExperienceSection = () => {
                                                 <h1 className='font-semibold txt-3xl'>{card.title}</h1>
                                                 <p className='my-5 text-white-50'>{card.date}</p>
 
-                                                <p className="text-[#839cb5] italic">
+                                                <p className="text-[#9cc3eb] italic">
                                                     Responsibilities
                                                 </p>
                                                 <ul className='list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50'>
