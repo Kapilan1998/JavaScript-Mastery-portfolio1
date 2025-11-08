13540ec94d78a9c3420bba960c317a447672baeb
Kapilan <sriranjankapilan@gmail.com>
2025-11-08 14:21:13 +0530

    gsap effect and other technologies failiar added

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,34 +1,78 @@
 import TitleHeader from '../components/TitleHeader'
-import { techStackIcons } from '../constants'
+import { techStackIcons, techStackImgs } from '../constants'
 import TechIcon from '../components/Models/TechLogos/TechIcon'
+import { useGSAP } from '@gsap/react'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger);
 
 const TechStack = () => {
+    useGSAP(() => {
+        gsap.fromTo(
+            '.tech-card',
+            { y: 50, opacity: 0 },
+            {
+                y: 0,
+                opacity: 1,
+                ease: 'power2.inOut',
+                stagger: 0.2,
+                scrollTrigger: {
+                    trigger: '#skills',
+                    start: 'top center',
+                },
+            }
+        )
+    })
+
     return (
-        <div id="skills" className='flex-center section-padding'>
-            <div className='w-full h-full md:px-10 px-5'>
+        <div id="skills" className="flex-center section-padding">
+            <div className="w-full h-full md:px-10 px-5">
                 <TitleHeader
                     title="My Preferred Tech Stack"
                     sub="Technologies I work with"
                 />
-                <div className='tech-grid'>
+
+                <div className="tech-grid">
                     {techStackIcons.map((icon) => (
-                        <div key={icon.name} className='card-border tech-card 
-                        overflow-hidden group xl:rounded-full rounded-lg'>
-                            <div className='tech-card-animated-bg' />
-                            <div className='tech-card-content'>
-                                <div className='tech-icon-wrapper'>
+                        <div
+                            key={icon.name}
+                            className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg"
+                        >
+                            <div className="tech-card-animated-bg" />
+                            <div className="tech-card-content">
+                                <div className="tech-icon-wrapper w-24 h-24">
                                     <TechIcon model={icon} />
                                 </div>
-
-                                <div className='padding-x w-full'>
+                                <div className="padding-x w-full">
                                     <p>{icon.name}</p>
                                 </div>
                             </div>
                         </div>
                     ))}
+
+
+                    {/* adding more Technologies which i am familiar with */}
+                    {techStackImgs.map((img) => (
+                        <div
+                            key={img.name}
+                            className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg"
+                        >
+                            <div className="tech-card-animated-bg" />
+                            <div className="tech-card-content">
+                                <div className="tech-icon-wrapper w-24 h-24 flex justify-center items-center">
+                                    <img src={img.imgPath} alt={img.name} />
+                                </div>
+                                <div className="padding-x w-full">
+                                    <p>{img.name}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+
+
                 </div>
             </div>
-
         </div>
     )
 }
