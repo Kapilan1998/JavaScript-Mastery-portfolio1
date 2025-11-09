193c846334262aa331cabfd6086e470f397575f4
Kapilan <sriranjankapilan@gmail.com>
2025-11-09 10:17:56 +0530

    indentation corrected

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -11,8 +11,8 @@ const Testimonials = () => {
                     sub="Client Feedbaack highlights" />
 
                 <div className='lg:columns-3 md:columns-2 columns-1 mt-16'>
-                    {testimonials.map(({imgPath, name, mentions,review}) => (
-                        <GlowCard card={{review}}>
+                    {testimonials.map(({ imgPath, name, mentions, review }) => (
+                        <GlowCard card={{ review }}>
                             <div className='flex items-center gap-3'>
                                 <div>
                                     <img src={imgPath} alt={name} />
